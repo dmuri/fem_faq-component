122889339f2fa46adf390a3e6deae2233de2b6d5
dmuri <72742970+dmuri@users.noreply.github.com>
2023-08-08 18:49:45 +0200

    increase button press area

--- components/questionanswer.tsx
+++ components/questionanswer.tsx
@@ -15,14 +15,14 @@ const QuestionAnswer: React.FC<QuestionAnswerProps> = ({question, answer}) => {
 
   return (
       <>
-        <div className="flex flex-col gap-1 mb-3">
-          <div className="flex justify-between items-center " onClick={handleClick} >
+        <div className="z-10 flex flex-col gap-1 mb-3 cursor-pointer" onClick={handleClick}>
+          <div className="flex justify-between items-center hover:cursor-pointer"  >
             <h2 className={`${isOpen ? 'font-bold' : ''} text-md text-neutral-very-dark-grayish-blue`}>{question}</h2>
             <button className="h-10 w-10 flex basis-auto justify-center items-center " >
               <img className={`${isOpen ? 'transform rotate-180' : ''} h-3 w-4`} src="/images/icon-arrow-down.svg" alt="" />
             </button>
           </div>
-          <p className={`${isOpen ? 'block' : 'hidden'} max-w-[320px]  md:max-w-[40ch] text-neutral-very-dark-grayish-blue`}>{answer}</p>
+          <p className={`${isOpen ? 'block' : 'hidden'}  max-w-[320px]  md:max-w-[40ch] text-neutral-very-dark-grayish-blue`}>{answer}</p>
           <hr className="mt-2"/>
         </div>
       </>
